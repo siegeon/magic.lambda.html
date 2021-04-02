32077f16f585972716d5ef1528d2e4700d040873
Thomas Hansen <thomas@gaiasoul.com>
2021-04-02 12:20:32 +0300

    Adding unit tests

---
+++ magic.lambda.html.tests/Common.cs
@@ -0,0 +1,65 @@
+/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - 2020, thomas@servergardens.com, all rights reserved.
+ * See the enclosed LICENSE file for details.
+ */
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using magic.node;
+using magic.signals.services;
+using magic.signals.contracts;
+using magic.node.extensions.hyperlambda;
+
+namespace magic.lambda.html.tests
+{
+    public static class Common
+    {
+        static public ISignaler GetSignaler()
+        {
+            var services = Initialize();
+            return services.GetService(typeof(ISignaler)) as ISignaler;
+        }
+
+        static public Node Evaluate(string hl)
+        {
+            var lambda = new Parser(hl).Lambda();
+            var signaler = Initialize().GetService(typeof(ISignaler)) as ISignaler;
+            signaler.Signal("eval", lambda);
+            return lambda;
+        }
+
+        #region [ -- Private helper methods -- ]
+
+        static IServiceProvider Initialize()
+        {
+            var configuration = new ConfigurationBuilder().Build();
+            var services = new ServiceCollection();
+            services.AddTransient<IConfiguration>((svc) => configuration);
+            services.AddTransient<ISignaler, Signaler>();
+            var types = new SignalsProvider(InstantiateAllTypes<ISlot>(services));
+            services.AddTransient<ISignalsProvider>((svc) => types);
+            var provider = services.BuildServiceProvider();
+            return provider;
+        }
+
+        static IEnumerable<Type> InstantiateAllTypes<T>(ServiceCollection services) where T : class
+        {
+            var type = typeof(T);
+            var result = AppDomain.CurrentDomain.GetAssemblies()
+                .Where(x => !x.IsDynamic && !x.FullName.StartsWith("Microsoft", StringComparison.InvariantCulture))
+                .SelectMany(s => s.GetTypes())
+                .Where(p => type.IsAssignableFrom(p) && !p.IsInterface && !p.IsAbstract);
+
+            foreach (var idx in result)
+            {
+                services.AddTransient(idx);
+            }
+            return result;
+        }
+
+        #endregion
+    }
+}
---
+++ magic.lambda.html.tests/HtmlTests.cs
@@ -0,0 +1,30 @@
+/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - 2020, thomas@servergardens.com, all rights reserved.
+ * See the enclosed LICENSE file for details.
+ */
+
+using System.Linq;
+using Xunit;
+using magic.node.extensions;
+
+namespace magic.lambda.html.tests
+{
+    public class HtmlTests
+    {
+        [Fact]
+        public void FromHtml()
+        {
+            var result = Common.Evaluate(@"
+html2lambda:@""<html>
+  <head>
+    <title>Foo</title>
+  </head>
+  <body>
+    <p class=""""howdy"""">
+  </body>
+</html>""");
+            Assert.Equal("Foo", new Expression("**/html2lambda/*/*/*/title/*/\\#text").Evaluate(result).First().Value);
+            Assert.Equal("howdy", new Expression("**/html2lambda/*/*/*/p/*/\\@class").Evaluate(result).First().Value);
+        }
+    }
+}
